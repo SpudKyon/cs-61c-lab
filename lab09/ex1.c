d27d02d64126e2c0356ae7e4eff9e215a3ef0369
SpudKyon <gangsta98t@gmail.com>
2023-10-26 12:51:47 +0800

    finish lab09

--- lab09/ex1.c
+++ lab09/ex1.c
@@ -12,8 +12,7 @@
 void hello_world(int message) {
     int procID;
 
-    // TODO: get the ID of the current program, and store in `procID`
-
+    MPI_Comm_rank(MPI_COMM_WORLD, &procID);
     // Sleep for message % 5 seconds
     sleep(message % 5);
 
@@ -24,58 +23,42 @@ void hello_world(int message) {
 int main(int argc, char** argv) {
     int numTasks = atoi(argv[1]);
 
-    // TODO: use MPI_Init to initialize the program
-
+    MPI_Init(&argc, &argv);
     int procID, totalProcs;
-    // TODO: get the total number of processes and store in `totalProcs`
-
-    // TODO: get the ID of the current program, and store in `procID`
-
-    // TODO: check if the current process is the manager
-    if (__________) {
+    MPI_Comm_size(MPI_COMM_WORLD, &totalProcs);
+    MPI_Comm_rank(MPI_COMM_WORLD, &procID);
+    if (procID == 0) {
         // Manager node
         int nextTask = 0;
         MPI_Status status;
         int32_t message;
 
-        // TODO: loop until we've completed `numTasks`
-        while (__________) {
-            // TODO: receive a message from any source (so we know that this node is done with its task)
-
-            // TODO: get the source process using the `status` struct
-
-            // TODO: send `nextTask` as the message to the process we just received a message from
-
-            // TODO: increment `nextTask` by 1
+        while (nextTask < numTasks) {
+            MPI_Recv(&message, 1, MPI_INT32_T, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+            int workerID = status.MPI_SOURCE;
+            MPI_Send(&nextTask, 1, MPI_INT32_T, workerID, 0, MPI_COMM_WORLD);
+            nextTask++;
         }
 
         // Wait for all processes to finish
-        // TODO: loop through all processes
-        // Hint: we have `totalProcs - 1` total, since there's a manager node
-        for (________________________________________) {
-            // TODO: receive a message from any source (so we know that this node is done with its task)
-
-            // TODO: get the source process using the `status` struct
-
-            // TODO: send `TERMINATE` as the message to the process we just received a message from
-
+        for (int i = 1; i < totalProcs; i++) {
+            MPI_Recv(&message, 1, MPI_INT32_T, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+            int workerID = status.MPI_SOURCE;
+            int terminate = TERMINATE;
+            MPI_Send(&terminate, 1, MPI_INT32_T, workerID, 0, MPI_COMM_WORLD);
         }
     } else {
         // Worker node
         int32_t message;
 
         while (true) {
-            // TODO: let the manager node know that this worker is ready
-            // Hint: use MPI_Send to send a message
-
-            // TODO: receive 1 message from the manager and store it in `message`
-            // Hint: use MPI_Recv to receive a message
-
-            // TODO: if the `message` is TERMINATE, break out of the loop to terminate
-
-            // TODO: call `hello_world` and pass `message` as the argument
+            message = READY;
+            MPI_Send(&message, 1, MPI_INT32_T, 0, 0, MPI_COMM_WORLD);
+            MPI_Recv(&message, 1, MPI_INT32_T, 0, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            if (message == TERMINATE)
+                break;
+            hello_world(message);
         }
     }
-
-    // TODO: call MPI_Finalize since it is the end of the program
+    MPI_Finalize();
 }
